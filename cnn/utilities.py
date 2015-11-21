068a2f6a9becca9c5df04cc604cb4cbeaeba686c
alfredolainez <alainez@stanford.edu>
2015-11-21 15:33:11 -0800

    Tokenization, text to glove vectors

--- cnn/utilities.py
+++ cnn/utilities.py
@@ -1,6 +1,7 @@
-'''
-sequences.py -- utilities for sequences [WIP]
-'''
+import spacy.en
+
+tokenizer = spacy.en.English()
+
 def normalize_sos(sq, sz=30):
     '''
     Take a list of lists and ensure that they are all of length `sz`
@@ -13,3 +14,24 @@ def normalize_sos(sq, sz=30):
     def _normalize(e, sz):
         return e[:sz] if len(e) >= sz else e + [0] * (sz - len(e))
     return [_normalize(e, sz) for e in sq]
+
+def tokenize_text(text):
+    if not isinstance(text, unicode):
+        text = unicode(text)
+
+    tokens = [token.lower_ for token in tokenizer(text)]
+
+    return tokens
+
+def to_glove_vectors(text, glovebox):
+    tokens = tokenize_text(text)
+
+    wvs = []
+    for token in tokens:
+        wvs.append(glovebox[token])
+
+    return wvs
+
+
+
+
