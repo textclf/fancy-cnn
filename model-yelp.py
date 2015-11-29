a6cb566f289bb0a95f15fdd537adc420f14c8d32
alfredolainez <alainez@stanford.edu>
2015-11-28 22:40:00 -0800

    Yelp data prepared for convolution, model with yelp data

---
+++ model-yelp.py
@@ -0,0 +1,141 @@
+import logging
+from keras.layers.recurrent import LSTM, GRU
+from keras.models import Sequential, model_from_json, Graph
+from keras.layers.core import Dense, Dropout, MaxoutDense, Activation
+from keras.layers.advanced_activations import PReLU
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+from keras.optimizers import SGD
+
+from cnn.layers.convolutions import *
+from cnn.layers.embeddings import *
+
+import numpy as np
+import cPickle as pickle
+
+LOGGER_PREFIX = ' %s'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def log(msg, logger=logger):
+    logger.info(LOGGER_PREFIX % msg)
+
+
+if __name__ == '__main__':
+
+    WV_FILE = './data/wv/IMDB-GloVe-100dim-glovebox.pkl'
+    MODEL_FILE = './test-model.h5'
+
+    # -- load in all the data
+    train, test = {}, {}
+
+    log('Loading training data')
+
+    train['text'] = np.load('Yelp_train_glove_X.npy')
+    train['labels'] = np.load('Yelp_train_glove_y.npy')
+
+    log('Shuffling training data')
+    shuff = range(train['text'].shape[0])
+    np.random.shuffle(shuff)
+
+    train['text'], train['labels'] = train['text'][shuff], train['labels'][shuff]
+
+    # -- flatten across paragraph dimension, will later be reconstructed in the embedding
+    train['text'] = train['text'].reshape(train['text'].shape[0], -1)
+
+    weights = 1.0 * (train['text'] > 0)
+
+    del shuff
+
+    log('Loading testing data')
+
+    # -- testing data
+    test['text'] = np.load('Yelp_test_glove_X.npy')
+    test['text'] = test['text'].reshape(test['text'].shape[0], -1)
+    test['labels'] = np.load('Yelp_test_glove_y.npy')
+
+    log('Loading Yelp trained word vectors')
+    gb = pickle.load(open(WV_FILE, 'rb'))
+
+    WV_PARAMS = {
+		'floating_wv' :
+		{
+			'vocab_size' : gb.W.shape[0],
+			'init' : gb.W,
+			'fixed' : False
+		}
+	}
+
+    NGRAMS = [2, 3, 4]
+    NFILTERS = 21
+    SENTENCE_LENGTH = 20
+    PARAGRAPH_LENGTH = 20
+
+    log('Making graph model')
+    graph = Graph()
+    graph.add_input(name='text', input_shape=(-1, ), dtype='int')
+
+    log('Making embedding')
+
+    embed = paragraph_embedding(PARAGRAPH_LENGTH, WV_PARAMS, WV_PARAMS['floating_wv']['init'].shape[1])
+
+    graph.add_node(embed, name='embedding', input='text')
+
+    # graph.add_node(Embedding(WV_PARAMS['floating_wv']['vocab_size'], ), name='embedding', input='text')
+
+    log('Adding convolved n-grams')
+    # for n in [4, 5]:
+    for n in NGRAMS:
+        graph.add_node(
+            TimeDistributedConvolution2D(NFILTERS, n, WV_PARAMS['floating_wv']['init'].shape[1], activation='relu'),
+			name='conv{}gram'.format(n), input='embedding')
+
+        graph.add_node(
+			TimeDistributedMaxPooling2D(pool_size=(SENTENCE_LENGTH - n + 1, 1)),
+			name='maxpool{}gram'.format(n), input='conv{}gram'.format(n))
+
+        graph.add_node(
+			Dropout(0.7),
+			name='dropout{}gram'.format(n), input='maxpool{}gram'.format(n))
+
+        graph.add_node(
+			TimeDistributedFlatten(),
+			name='flatten{}gram'.format(n), input='dropout{}gram'.format(n))
+
+    log('Adding bi-directional GRU')
+    graph.add_node(GRU(25), name='gru_forwards', inputs=['flatten{}gram'.format(n) for n in NGRAMS], concat_axis=-1)
+    # graph.add_node(GRU(25, go_backwards=True), name='gru_backwards', inputs=['flatten{}gram'.format(n) for n in NGRAMS], concat_axis=-1)
+    # graph.add_node(GRU(16), name='gru', input='flatten4gram')
+
+    graph.add_node(Dropout(0.5), name='gru_dropout', input='gru_forwards')
+
+    graph.add_node(Dense(1, activation='sigmoid'), name='probability', input='gru_dropout')
+
+    graph.add_output(name='prediction', input='probability')
+
+    log('Compiling model (Veuillez patienter)...')
+    graph.compile('rmsprop', {'prediction': 'binary_crossentropy'})
+
+    log('Fitting! Hit CTRL-C to stop early...')
+    try:
+        history = graph.fit(
+			{'text': train['text'], 'prediction': train['labels']},
+			validation_split=0.35, batch_size=28, nb_epoch=5,
+			sample_weight = {'prediction' : weights}, callbacks =
+				   [
+				       EarlyStopping(verbose=True, patience=30, monitor='val_loss'),
+				       ModelCheckpoint(MODEL_FILE, monitor='val_loss', verbose=True, save_best_only=True)
+				   ]
+		   )
+    except KeyboardInterrupt:
+        log('Training stopped early!')
+
+    log('Loading best weights...')
+    graph.load_weights(MODEL_FILE)
+
+    log('getting predictions on the test set')
+    yhat = graph.predict({'text': test['text']}, verbose=True, batch_size=50)
+
+    acc = ((yhat['prediction'].ravel() > 0.5) == (test['labels'] > 0.5)).mean()
+
+    log('Test set accuracy of {}%.'.format(acc * 100.0))
+    log('Test set error of {}%. Exiting...'.format((1 - acc) * 100.0))
---
+++ prepare-yelp.py
@@ -0,0 +1,125 @@
+'''
+prepare-yelp.py
+
+description: prepare the yelp data for training in DNNs
+'''
+
+import cPickle as pickle
+import logging
+import numpy as np
+from multiprocessing import Pool
+from cnn.wordvectors.glove import GloVeBox
+from util.misc import normalize_sos
+
+LOGGER_PREFIX = ' %s'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+def log(msg, logger=logger):
+    logger.info(LOGGER_PREFIX % msg)
+
+def parse_paragraph(txt):
+    '''
+    Takes a text and returns a list of lists of tokens, where each sublist is a sentence
+    '''
+    return [[t.text for t in s] for s in nlp(txt).sents]
+
+def parallel_run(f, parms):
+    '''
+    performs multi-core map of the function `f`
+    over the parameter space spanned by parms.
+
+    `f` MUST take only one argument.
+    '''
+    pool = Pool()
+    ret = pool.map(f, parms)
+    pool.close()
+    pool.join()
+    return ret
+
+TRAIN_FILE = "datasets/yelp/data_funny_binary_balanced/TrainSet_147444"
+DEV_FILE = "datasets/yelp/data_funny_binary_balanced/DevSet_147444"
+TEST_FILE = "datasets/yelp/data_funny_binary_balanced/TestSet_147444"
+
+NUM_TRAIN_REVIEWS = 30000 # None if want to use all
+NUM_TEST_REVIEWS = 10000
+
+# -- parameters to tune and set
+WORDS_PER_SENTENCE = 20
+SENTENCES_PER_PARAGRAPH = 20
+
+WV_FILE = './data/wv/IMDB-GloVe-100dim.txt'
+
+log('Importing spaCy...')
+from spacy.en import English
+
+log('Initializing spaCy...')
+nlp = English()
+
+if __name__ == '__main__':
+
+    log('Building word vectors from {}'.format(WV_FILE))
+    gb = GloVeBox(WV_FILE)
+    gb.build(zero_token=True).index()
+
+    log('writing GloVeBox pickle...')
+    pickle.dump(gb, open(WV_FILE.replace('.txt', '-glovebox.pkl'), 'wb'), pickle.HIGHEST_PROTOCOL)
+
+    log('Loading train and test pickles...')
+
+    with open(TRAIN_FILE) as file:
+        [train_reviews, train_labels] = pickle.load(file)
+    with open(DEV_FILE) as file:
+        [dev_reviews, dev_labels] = pickle.load(file)
+    with open(TEST_FILE) as file:
+        [test_reviews, test_labels] = pickle.load(file)
+
+    # Merge train and dev
+    train_reviews.extend(dev_reviews)
+    train_reviews = train_reviews[:NUM_TRAIN_REVIEWS]
+    train_labels.extend(dev_labels)
+    train_labels = train_labels[:NUM_TRAIN_REVIEWS]
+
+    test_reviews = test_reviews[:NUM_TEST_REVIEWS]
+    test_labels = test_labels[:NUM_TEST_REVIEWS]
+
+    log('Splitting training data into paragraphs')
+    train_text_sentences = parallel_run(parse_paragraph, train_reviews)
+    test_text_sentences = parallel_run(parse_paragraph, test_reviews)
+
+    log('normalizing training inputs...')
+    train_repr = normalize_sos(
+						[
+								normalize_sos(review, WORDS_PER_SENTENCE)
+								for review in gb.get_indices(train_text_sentences)
+                        ],
+			SENTENCES_PER_PARAGRAPH, [0] * WORDS_PER_SENTENCE
+		)
+
+    train_text = np.array(train_repr)
+
+    log('normalizing testing inputs...')
+    test_repr = normalize_sos(
+						[
+							normalize_sos(review, WORDS_PER_SENTENCE)
+							for review in gb.get_indices(test_text_sentences)
+						],
+		SENTENCES_PER_PARAGRAPH, [0] * WORDS_PER_SENTENCE
+    )
+
+    test_text = np.array(test_repr)
+
+    log('Saving...')
+
+    # -- training data save
+    np.save('Yelp_train_glove_X.npy', train_text)
+    np.save('Yelp_train_glove_y.npy', train_labels)
+
+    # -- testing data save
+    np.save('Yelp_test_glove_X.npy', test_text)
+    np.save('Yelp_test_glove_y.npy', test_labels)
+
+
+
+
+
+
