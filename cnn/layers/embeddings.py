8cd26f6b7f3584a1a356ca064d21b0b3e0774b10
lukedeo <lukepercivaldeoliveira@gmail.com>
2015-12-01 15:13:11 -0800

    removing shape inference for keras backend compatibility

--- cnn/layers/embeddings.py
+++ cnn/layers/embeddings.py
@@ -95,7 +95,7 @@ def sentence_embedding(sentence_len, wv_params, wv_size,
 
 
 
-def paragraph_embedding(sentence_len, wv_params, wv_size,
+def paragraph_embedding(paragraph_len, sentence_len, wv_params, wv_size,
         input_name='paragraph_embedding',  output_name='vector_embedding'):
     '''
     Creates an embedding of word vectors into a sequence of sentence images.
@@ -147,16 +147,18 @@ def paragraph_embedding(sentence_len, wv_params, wv_size,
     
     # -- fix for new keras backend
     # g.add_input(input_name, (-1, ), dtype='int')
-    g.add_input(input_name, (sentence_len * wv_size, ), dtype='int')
+    g.add_input(input_name, (sentence_len * paragraph_len, ), dtype='int')
     for name, params in wv_params.iteritems():
         # g.add_input(params['input_name'], (-1, ), dtype='int')
         g.add_node(make_embedding(wv_size=wv_size, **params), name=name, input=input_name)
 
     if len(wv_params.keys()) > 1:
-        g.add_node(Reshape((-1, sentence_len, len(wv_params), wv_size)),
+        # g.add_node(Reshape((-1, sentence_len, len(wv_params), wv_size)),
+        g.add_node(Reshape((paragraph_len, sentence_len, len(wv_params), wv_size)),
                    name='reshape', inputs=wv_params.keys(), merge_mode='concat')
     else:
-        g.add_node(Reshape((-1, sentence_len, len(wv_params), wv_size)),
+        # g.add_node(Reshape((-1, sentence_len, len(wv_params), wv_size)),
+        g.add_node(Reshape((paragraph_len, sentence_len, len(wv_params), wv_size)),
                    name='reshape', input=wv_params.keys()[0])
 
     g.add_node(Permute(dims=(1, 3, 2, 4)), name='permute', input='reshape')
