120d2a6a3e931539ca643b3f4f1559399cc4d49e
alfredolainez <alainez@stanford.edu>
2016-02-03 23:59:41 -0800

    Added grid search with crossvalidation. Some improvements

--- classic/classifiers.py
+++ classic/classifiers.py
@@ -9,35 +9,24 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
 from sklearn.svm import LinearSVC
+from sklearn.grid_search import GridSearchCV
 import matplotlib.pyplot as plt
 import numpy as np
-
-def plot_confusion_matrix(cm):
-    """
-    Plots a scikit learn confusion matrix
-    """
-    plt.matshow(cm)
-    plt.title('Confusion matrix')
-    plt.colorbar()
-    plt.ylabel('True category')
-    plt.xlabel('Predicted category')
-    plt.show()
+from time import strftime
 
 class TextClassifierException(Exception):
     pass
 
 class TextClassifier(object):
 
-    def __init__(self, ngram_range=(1,1),
-                 train_texts=None, train_labels=None,
-                 validation_texts=None, validation_labels=None,
+    def __init__(self, train_texts=None, train_labels=None,
+                 compute_features=False,
+                 train=False, ngram_range=(1,1),
                  test_texts=None, test_labels=None):
 
         self.ngram_range = ngram_range
         self.train_texts = train_texts
         self.train_labels = train_labels
-        self.validation_texts = validation_texts
-        self.validation_labels = validation_labels
         self.test_texts = test_texts
         self.test_labels = test_labels
 
@@ -47,17 +36,22 @@ class TextClassifier(object):
         # Classifier
         self.clf = None
 
-    def set_training_data(self, train_texts, train_labels):
+        if compute_features:
+            self.set_bag_of_ngrams()
+        if train:
+            self.train()
+
+    def set_training_data(self, train_texts, train_labels, compute_features=True):
         self.train_texts = train_texts
         self.train_labels = train_labels
 
-        # Feature vectors are not valid anymore
-        self.count_vect = None
-        self.tfidf_transformer = None
-
-    def set_validation_data(self, validation_texts, validation_labels):
-        self.validation_texts = validation_texts
-        self.validation_labels = validation_labels
+        if compute_features:
+            self.set_bag_of_ngrams()
+        else:
+            # Feature vectors are not valid anymore
+            self.count_vect = None
+            self.tfidf_transformer = None
+        self.clf = None
 
     def set_test_data(self, test_texts, test_labels):
         self.test_texts = test_texts
@@ -114,9 +108,29 @@ class TextClassifier(object):
             labels = self.test_labels
         X = self._to_features(texts)
         predicted = self.clf.predict(X)
-        self.cm = confusion_matrix(labels, predicted)
 
         return precision_recall_fscore_support(labels, predicted, average='macro')
+
+    def _grid_search(self, estimator, param_grid=None, scoring=None,
+                     n_jobs=1, cv=None, verbose=0):
+        """
+        See http://scikit-learn.org/stable/modules/generated/sklearn.grid_search.GridSearchCV.html
+        for description on parameters
+
+        Note: Watch out scoring function for unbalanced problems
+        """
+
+        print strftime("%Y-%m-%d %H:%M:%S") + ": Starting grid search for " + self.__class__.__name__ + \
+              ". n-gram range = " + str(self.ngram_range)
+
+        self.clf = GridSearchCV(estimator, param_grid=param_grid, scoring=scoring,
+                                n_jobs=n_jobs, cv=cv, verbose=verbose)
+        X = self._to_features(self.train_texts)
+        self.clf.fit(X, self.train_labels)
+
+        print strftime("%Y-%m-%d %H:%M:%S") + ": Finished grid search for " + self.__class__.__name__
+        print "Best error rate: " + str(1 - self.clf.best_score_)
+        print "Best params: " + str(self.clf.best_params_)
 
 
 class NaiveBayesClassifier(TextClassifier):
@@ -129,25 +143,55 @@ class NaiveBayesClassifier(TextClassifier):
         X = self._to_features(self.train_texts)
         self.clf = MultinomialNB().fit(X, self.train_labels)
 
+    def grid_search_cv(self, param_grid=None, scoring=None,
+                       n_jobs=1, cv=None, verbose=0):
+        if param_grid is None:
+            # Some default nice parameters
+            param_grid={'alpha': [0.1, 0.5, 1, 2, 3]}
+
+        self._grid_search(MultinomialNB(), param_grid, scoring, n_jobs, cv, verbose)
+
 class SGDTextClassifier(TextClassifier):
     """
     Stochastic Gradient Descent for text classification
     """
-    def train(self):
+    def train(self, loss="log", alpha=0.001, penalty="l2", n_iter=5):
         self._train_checks()
         X = self._to_features(self.train_texts)
-        self.clf = SGDClassifier(loss="modified_huber", alpha=0.001,
-                                 penalty="l2").fit(X, self.train_labels)
+        self.clf = SGDClassifier(loss=loss, alpha=alpha,
+                                 penalty=penalty,
+                                 n_iter=n_iter).fit(X, self.train_labels)
+
+    def grid_search_cv(self, param_grid=None, scoring=None,
+                       n_jobs=1, cv=None, verbose=0):
+        if param_grid is None:
+            # Some default nice parameters
+            param_grid={'alpha': 10.0**-np.arange(1,6),
+                        'loss': ['hinge', 'log', 'modified_huber', 'squared_hinge',
+                                 'perceptron','squared_loss','huber',
+                                 'epsilon_insensitive','squared_epsilon_insensitive'],
+                        'penalty': ['none', 'l2', 'l1', 'elasticnet']}
+
+        self._grid_search(SGDClassifier(), param_grid, scoring, n_jobs, cv, verbose)
 
 class LogisticClassifier(TextClassifier):
     """
     Logistic Regression for text classification
     """
 
-    def train(self):
+    def train(self, C=1.0, penalty="l2"):
         self._train_checks()
         X = self._to_features(self.train_texts)
-        self.clf = LogisticRegression(C=1e5).fit(X, self.train_labels)
+        self.clf = LogisticRegression(C=C, penalty=penalty).fit(X, self.train_labels)
+
+    def grid_search_cv(self, param_grid=None, scoring=None,
+                       n_jobs=1, cv=None, verbose=0):
+        if param_grid is None:
+            # Some default nice parameters
+            param_grid={'penalty': ['l1', 'l2'],
+                        'C': [1.0, 0.5, 0.25, 0.0125, 0.001, 0.0001, 5, 10, 50]}
+
+        self._grid_search(LogisticRegression(), param_grid, scoring, n_jobs, cv, verbose)
 
 class SVMClassifier(TextClassifier):
     """
